c8620705bcf0763841591b80d4536fe2fba2ef19
Nabeel Khan <nabeekha@umich.edu>
2023-07-30 11:14:27 -0400

    adding additional styling

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// This is where you can add any JavaScript functionality if needed.
-// For this example, we don't need any specific JavaScript behavior.
+// This javaScript file has basic functionalities such as button clicks that you can reference to edit your own website!
+
+// Get the YouTube channel button element
+const youtubeChannelButton = document.querySelector(".youtube-channel-button");
+
+// Add a click event listener to the button
+youtubeChannelButton.addEventListener("click", () => {
+  // Redirect the user to the YouTube channel link
+  window.location.href = "https://www.youtube.com/channel/UCXdSahmtHEhrD4ye6k7p8Aw";
+});

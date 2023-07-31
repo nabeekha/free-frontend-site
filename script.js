5fccdf9299f605f7b46e43be4f54fdbfab7a39d1
Nabeel Khan <nabeekha@umich.edu>
2023-07-31 18:23:56 -0400

    commit with full free site containing 3 pages that link to one another

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // This javaScript file has basic functionalities such as button clicks that you can reference to edit your own website!
 
-// Get the YouTube channel button element
-const youtubeChannelButton = document.querySelector(".youtube-channel-button");
+// Get the YouTube button element by its ID
+const youtubeBtn = document.getElementById('youtube-btn');
 
-// Add a click event listener to the button
-youtubeChannelButton.addEventListener("click", () => {
-  // Redirect the user to the YouTube channel link
-  window.location.href = "https://www.youtube.com/channel/UCXdSahmtHEhrD4ye6k7p8Aw";
-});
+// Function to open the YouTube channel link in a new tab
+function openYouTubeChannel() {
+    const youtubeChannelLink = "https://www.youtube.com/channel/UCXdSahmtHEhrD4ye6k7p8Aw";
+    window.open(youtubeChannelLink, '_blank');
+}
+
+// Add a click event listener to the YouTube button
+youtubeBtn.addEventListener('click', openYouTubeChannel);
